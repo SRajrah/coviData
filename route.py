b0e861c84ace9c29526076e35499db4ac9e1c8dc
srajrah <shubhamrajrah@gmail.com>
2021-05-02 01:08:38 +0530

    main structure of the project

--- route.py
+++ route.py
@@ -1,4 +1,4 @@
 from __main__ import app
 from controller.generateResources import generateString
 
-app.add_url_rule('/', view_func=generateString, methods=['POST'])
+app.add_url_rule('/', view_func=generateString, methods=['GET', 'POST'])
